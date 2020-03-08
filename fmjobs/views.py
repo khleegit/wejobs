aafe89ee76b2532ea9be36cd1d3278f4f08bd4d8
khleegit <kh.lee@corein.co.kr>
2020-03-08 16:42:18 +0900

    2020-03-08 16:41 change to admin

--- fmjobs/views.py
+++ fmjobs/views.py
@@ -3,8 +3,18 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def post_blank(request):
+    return render(request, 'fmjobs/post_blank.html', {})
+
+def login(request):
+    
+    return render(request, 'registration/login.html', {})
+
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'fmjobs/post_list.html', {'posts': posts})
@@ -39,3 +49,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'fmjobs/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
