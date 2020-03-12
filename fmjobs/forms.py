371ccc90dfec9884298cab3dc30fefd0d00a04d6
khleegit <kh.lee@corein.co.kr>
2020-03-12 16:58:09 +0900

    2020-03-12 16:57 edit post_edit.html

--- fmjobs/forms.py
+++ fmjobs/forms.py
@@ -7,4 +7,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('author', 'title', 'link1title', 'link1', 'link2title', 'link2', 'link3title', 'link3', 'link4title', 'link4', 'link5title', 'link5', 'text', 'monthlyincome', 'profitperiod', 'advantage', 'weakness',)
